04f4ae087306f01d4e09ea58c4d0bc8473bb89d9
cristiana-marcu <cristiana.marcu.n@gmail.com>
2021-08-26 18:12:45 +0200

    TODO: search for command path using split and refactor main to extract functions. Don't forget about norminette

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 13:26:28 by cmarcu            #+#    #+#             */
-/*   Updated: 2021/08/26 16:34:25 by cmarcu           ###   ########.fr       */
+/*   Updated: 2021/08/26 18:05:41 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,51 +19,59 @@ int	main(int argc, char **argv)
 	int out_file;
 	int status;
 
-	argc = 3;
-	pipe(pipefd);
-	pid = fork();
-	if (pid < 0)
+	if (argc == 5)
 	{
-		printf("Fork didn´t work");
-	}
-	else if (pid == 0) //proceso hijo
-	{
-		//Cerrar extremo de lectura
-		close(pipefd[R_END]);
-
-		//Duplicar el extremo de escritura para sustituir stdout dup2(pipefd[W], STDIN_FILENO)
-		dup2(pipefd[W_END], STDIN_FILENO);
-
-		//Cerrar el extremo que acabamos de duplicar
-		close(pipefd[W_END]);
-
-		//Ejecutar el comando
-		execlp("/bin/ls", "ls", "-la", NULL);
-	}
-	else //proceso padre
-	{
-		//Cerrar extremo de escritura del hijo
-		close(pipefd[W_END]);
-
-		//Crear otro hijo porque vamos a ejecutar otro exe
+		pipe(pipefd);
 		pid = fork();
-
-		if (pid == 0) //proceso hijo
+		if (pid == -1)
 		{
-			//Abrir archivo y redireccionar salida estandar al archivo
-			out_file = open(argv[3], O_WRONLY);
-			dup2(pipefd[R_END], STDIN_FILENO);
-			close(pipefd[R_END]);
-			dup2(out_file, STDOUT_FILENO);
-
-			//Ejecutar comando
-			execlp("usr/bin/wc", "wc", NULL);
+			printf("Fork didn´t work");
 		}
-		else //padre
+		else if (pid == 0) //proceso hijo
 		{
+			//Cerrar extremo de lectura
 			close(pipefd[R_END]);
+
+			//Duplicar el extremo de escritura para sustituir stdout dup2(pipefd[W], STDIN_FILENO)
+			dup2(pipefd[W_END], STDIN_FILENO);
+
+			//Cerrar el extremo que acabamos de duplicar
+			close(pipefd[W_END]);
+
+			//Ejecutar el comando
+			execlp("/bin/ls", "ls", "-la", NULL);
 		}
+		else //proceso padre
+		{
+			//Cerrar extremo de escritura del hijo
+			close(pipefd[W_END]);
+
+			//Crear otro hijo porque vamos a ejecutar otro exe
+			pid = fork();
+
+			if (pid == -1)
+			{
+				printf("Fork didn´t work");
+			}
+			else if (pid == 0) //proceso hijo
+			{
+				//Abrir archivo y redireccionar salida estandar al archivo
+				out_file = open(argv[3], O_WRONLY);
+				dup2(pipefd[R_END], STDIN_FILENO);
+				close(pipefd[R_END]);
+				dup2(out_file, STDOUT_FILENO);
+
+				//Ejecutar comando
+				execlp("usr/bin/wc", "wc", NULL);
+			}
+			else //padre
+			{
+				close(pipefd[R_END]);
+			}
+		}
+		wait(&status);
+		wait(&status);
 	}
-	wait(&status);
-	wait(&status);
+	else
+		printf("Wrong formatting");
 }
