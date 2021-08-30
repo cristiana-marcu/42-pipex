6c09baa98bdc550850a0ec4df6610e30e838f9d6
cristiana-marcu <cristiana.marcu.n@gmail.com>
2021-08-30 17:24:05 +0200

    Refactored to have error checking first thing

--- pipex.c
+++ pipex.c
@@ -6,72 +6,84 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 13:26:28 by cmarcu            #+#    #+#             */
-/*   Updated: 2021/08/26 18:05:41 by cmarcu           ###   ########.fr       */
+/*   Updated: 2021/08/30 17:22:01 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+void pipex(int infile, int outfile, char **argv, char **envp)
 {
-	int pipefd[2];
-	pid_t pid;
-	int out_file;
+	int end[2];
+	pid_t child1;
+	pid_t child2;
 	int status;
 
-	if (argc == 5)
+	pipe(end);
+	child1 = fork();
+	if (child1 == -1)
 	{
-		pipe(pipefd);
-		pid = fork();
-		if (pid == -1)
+		return (perror("Fork: "));
+	}
+	else if (child1 == 0) //proceso hijo
+	{
+		//Cerrar extremo de lectura
+		close(end[R]);
+
+		//Duplicar el extremo de escritura para sustituir stdout dup2(pipefd[W], STDIN_FILENO)
+		dup2(end[W], STDIN_FILENO);
+
+		//Cerrar el extremo que acabamos de duplicar
+		close(end[W]);
+
+		//Ejecutar el comando
+		execlp("/bin/ls", "ls", "-la", NULL);
+	}
+	else //proceso padre
+	{
+		//Cerrar extremo de escritura del hijo
+		close(end[W]);
+
+		//Crear otro hijo porque vamos a ejecutar otro exe
+		child2 = fork();
+
+		if (child2 == -1)
 		{
 			printf("Fork didn´t work");
 		}
-		else if (pid == 0) //proceso hijo
+		else if (child2 == 0) //proceso hijo
 		{
-			//Cerrar extremo de lectura
-			close(pipefd[R_END]);
+			//Abrir archivo y redireccionar salida estandar al archivo
+			dup2(end[R], STDIN_FILENO);
+			close(end[R]);
+			dup2(outfile, STDOUT_FILENO);
 
-			//Duplicar el extremo de escritura para sustituir stdout dup2(pipefd[W], STDIN_FILENO)
-			dup2(pipefd[W_END], STDIN_FILENO);
-
-			//Cerrar el extremo que acabamos de duplicar
-			close(pipefd[W_END]);
-
-			//Ejecutar el comando
-			execlp("/bin/ls", "ls", "-la", NULL);
+			//Ejecutar comando
+			execlp("usr/bin/wc", "wc", NULL);
 		}
-		else //proceso padre
+		else //padre
 		{
-			//Cerrar extremo de escritura del hijo
-			close(pipefd[W_END]);
-
-			//Crear otro hijo porque vamos a ejecutar otro exe
-			pid = fork();
-
-			if (pid == -1)
-			{
-				printf("Fork didn´t work");
-			}
-			else if (pid == 0) //proceso hijo
-			{
-				//Abrir archivo y redireccionar salida estandar al archivo
-				out_file = open(argv[3], O_WRONLY);
-				dup2(pipefd[R_END], STDIN_FILENO);
-				close(pipefd[R_END]);
-				dup2(out_file, STDOUT_FILENO);
-
-				//Ejecutar comando
-				execlp("usr/bin/wc", "wc", NULL);
-			}
-			else //padre
-			{
-				close(pipefd[R_END]);
-			}
+			close(end[R]);
 		}
-		wait(&status);
-		wait(&status);
 	}
-	else
-		printf("Wrong formatting");
+	wait(&status);
+	wait(&status);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int infile;
+	int outfile;
+
+	infile = open(argv[1], O_RDONLY);
+	outfile = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (infile < 0 || outfile < 0)
+		return (-1);
+	if (argc != 5)
+		return (-1);
+	pipex(infile, outfile, argv, envp);
+
+
+	/*else
+		printf("Wrong formatting");*/
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 13:41:01 by cmarcu            #+#    #+#             */
-/*   Updated: 2021/08/26 16:30:33 by cmarcu           ###   ########.fr       */
+/*   Updated: 2021/08/30 17:15:02 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
-# define W_END 1
-# define R_END 0
+# define W 1
+# define R 0
+
+void pipex(int infile, int outfile, char **argv, char **envp);
 
 #endif
