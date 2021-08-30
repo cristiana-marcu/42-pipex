bb6e3ed6223c10e5bb44e0474cc4788cfbcac1ec
cristiana-marcu <cristiana.marcu.n@gmail.com>
2021-08-30 18:10:40 +0200

    Using execve instead of excelp. TODO: parsing

--- pipex.c
+++ pipex.c
@@ -6,13 +6,13 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 13:26:28 by cmarcu            #+#    #+#             */
-/*   Updated: 2021/08/30 17:22:01 by cmarcu           ###   ########.fr       */
+/*   Updated: 2021/08/30 18:09:17 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void pipex(int infile, int outfile, char **argv, char **envp)
+void pipex(int infile, int outfile, char **argv, char **envp) // , char **argv, char **envp
 {
 	int end[2];
 	pid_t child1;
@@ -22,44 +22,34 @@ void pipex(int infile, int outfile, char **argv, char **envp)
 	pipe(end);
 	child1 = fork();
 	if (child1 == -1)
-	{
 		return (perror("Fork: "));
-	}
 	else if (child1 == 0) //proceso hijo
 	{
-		//Cerrar extremo de lectura
 		close(end[R]);
 
-		//Duplicar el extremo de escritura para sustituir stdout dup2(pipefd[W], STDIN_FILENO)
-		dup2(end[W], STDIN_FILENO);
-
-		//Cerrar el extremo que acabamos de duplicar
-		close(end[W]);
+		dup2(infile, STDIN_FILENO);
+		dup2(end[W], STDOUT_FILENO);
+		close(infile);
 
 		//Ejecutar el comando
-		execlp("/bin/ls", "ls", "-la", NULL);
+		execve("/bin/ls", &argv[2], envp);
 	}
 	else //proceso padre
 	{
-		//Cerrar extremo de escritura del hijo
 		close(end[W]);
 
-		//Crear otro hijo porque vamos a ejecutar otro exe
 		child2 = fork();
-
 		if (child2 == -1)
-		{
-			printf("Fork didn´t work");
-		}
+			return (perror("Fork: "));
 		else if (child2 == 0) //proceso hijo
 		{
-			//Abrir archivo y redireccionar salida estandar al archivo
 			dup2(end[R], STDIN_FILENO);
 			close(end[R]);
 			dup2(outfile, STDOUT_FILENO);
+			close(outfile);
 
 			//Ejecutar comando
-			execlp("usr/bin/wc", "wc", NULL);
+			execve("/usr/bin/wc", &argv[3], envp);
 		}
 		else //padre
 		{
@@ -78,12 +68,14 @@ int	main(int argc, char **argv, char **envp)
 	infile = open(argv[1], O_RDONLY);
 	outfile = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (infile < 0 || outfile < 0)
+	{
+		perror("Error: ");
 		return (-1);
+	}
 	if (argc != 5)
+	{
+		printf("Wrong formatting");
 		return (-1);
+	}
 	pipex(infile, outfile, argv, envp);
-
-
-	/*else
-		printf("Wrong formatting");*/
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 13:41:01 by cmarcu            #+#    #+#             */
-/*   Updated: 2021/08/30 17:15:02 by cmarcu           ###   ########.fr       */
+/*   Updated: 2021/08/30 18:03:36 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@
 # define W 1
 # define R 0
 
-void pipex(int infile, int outfile, char **argv, char **envp);
+void pipex(int infile, int outfile, char **argv, char **envp); //, char **argv, char **envp
 
 #endif
